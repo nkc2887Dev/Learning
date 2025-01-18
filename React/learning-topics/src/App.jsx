a7673853de00ea2bf11baf22391fbfea2fb28749
Naresh Chudasama <nkc2887dev@gmail.com>
2025-01-18 11:17:12 +0530

    useReducer

---
+++ React/learning-topics/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Reducer from "./components/Reducer";
+
+function App() {
+
+  return (
+     <>
+      <Reducer />
+      </>
+  );
+}
+
+export default App;
---
+++ React/learning-topics/src/components/Reducer.jsx
@@ -0,0 +1,67 @@
+import { useReducer } from "react";
+
+const ACTIONS = {
+  ADD: "addItems",
+  REMOVE: "removeItems",
+  RESET: "reset",
+};
+
+const itemReducer = (items, action) => {
+  switch (action.type) {
+    case ACTIONS.ADD:
+      return [...items, action.data];
+    case ACTIONS.REMOVE:
+      return items.filter((item) => item.id !== action.id);
+    case ACTIONS.RESET:
+      return [];
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+};
+
+const generateRandomName = () => {
+  const names = ["Bottle", "Mobile", "Tablet", "Laptop", "Speaker", "Headphone"];
+  const randomIndex = Math.floor(Math.random() * names.length);
+  return names[randomIndex];
+};
+
+function Reducer() {
+  const [data, dispatch] = useReducer(itemReducer, [{ id: 1, name: "Mobile" }]);
+
+  const addItem = () => {
+    dispatch({
+      type: ACTIONS.ADD,
+      data: { id: Date.now(), name: generateRandomName() },
+    });
+  };
+
+  const removeItem = (id) => {
+    dispatch({ type: ACTIONS.REMOVE, id });
+  };
+
+  const resetItems = () => {
+    dispatch({ type: ACTIONS.RESET });
+  };
+
+  return (
+    <div className="card">
+      <h3>
+        <i>useReducer Example</i>
+      </h3>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id} className="item">
+            {item.name}
+            <div className="actions">
+              <button onClick={addItem}>Add</button>
+              <button onClick={() => removeItem(item.id)}>Remove</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <button onClick={resetItems} className="reset-btn">Reset All</button>
+    </div>
+  );
+}
+
+export default Reducer;
